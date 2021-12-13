a2adb8b9f246a9f56bcff6afe16921b2bd6efa74
Adan Figueroa <fija90@gmail.com>
2021-12-13 11:30:09 -0600

    request api issue

--- api/controllers/songs/index.js
+++ api/controllers/songs/index.js
@@ -4,20 +4,48 @@ import { getResults } from '../../services/songs.js';
 
 const routes = express.Router();
 
-routes.post('/', async (req, res) => {
+routes.get('/', async (req, res) => {
     try {
-        const authHeader = req.headers['authorization'];
-        const token = authHeader && authHeader.split(' ')[1];
-        if (token == null) return res.send('No token on request').statusCode(401);
-        if(!verify) return res.send('Invalid Token').statusCode(403);
-        const { term } = req.body;
-        res.send(await getResults(term));
+        // const authHeader = req.headers['authorization'];
+        // const token = authHeader && authHeader.split(' ')[1];
+        console.log('entra');
+        // if (token == null) return res.send('No token on request').status(401);
+        // if(!verify) return res.send('Invalid Token').status(403);
+        const term  = 'Queen';
+        console.log(term);
+        console.log(await getResults(term));
+        const finalData = await getResults(term);
+        res.send(finalData);
     } catch (error) {
+        console.log(error);
         res.send({
             message: 'Error on search API',
-            statusCode: 500,
+            status: 500,
         });
     }
 });
+
+routes.post('/', async (req, res) => {
+    try {
+        // const authHeader = req.headers['authorization'];
+        // const token = authHeader && authHeader.split(' ')[1];
+        console.log('entra');
+        // if (token == null) return res.send('No token on request').status(401);
+        // if(!verify) return res.send('Invalid Token').status(403);
+        const { term } = req.body;
+        console.log(term);
+        console.log(await getResults(term));
+        const finalData = await getResults(term);
+        res.send(finalData);
+    } catch (error) {
+        console.log(error);
+        res.send({
+            message: 'Error on search API',
+            status: 500,
+        });
+    }
+});
+
+
 
 export default routes;
--- front/src/components/Search.js
+++ front/src/components/Search.js
@@ -1,18 +1,50 @@
 
-import React from 'react';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 
-function Search() {
+const Search = () => {
+    const [songsData, setSongsData] = useState({});
+    const [term, setTerm] = useState({});
 
-  return(
-    <><h2>Search</h2><form>
-          <label>
-              <p>Search</p>
-              <input type="text" />
-          </label>
-          <div>
-              <button type="submit">Submit</button>
-          </div>
-      </form></>
-  );
+    const getToken = () => {
+        const tokenString = localStorage.getItem('token');
+        const userToken = JSON.parse(tokenString);
+        return userToken?.token
+    };
+
+    const searchAPI = () => {
+        console.log('entra');
+        const token = `Bearer ${getToken()}`;
+        console.log(token);
+        axios({
+                url: 'http://localhost:3000/songs',
+                method: 'GET'
+                // headers: {
+                //     'Authorization': token,
+                //   },
+                // method: "POST",
+                // body: {term: 'Mac Miller'}
+            }).then(res => console.log(res))
+            .catch(error => console.log(error));       
+        console.log('finalizo');
+    }
+    return(
+        <><h2>Search</h2><form>
+            <label>
+                <p>Search</p>
+                <input type="text" onChange={e => setTerm(e.target.value)}/>
+            </label>
+            <div>
+                <button type="submit" onClick={searchAPI}>Submit</button>
+            </div>
+        </form>
+        {/* <ul>
+          {songsData.artistData.map(artist => {
+            return <li key={`name-${artist.name}`}>{artist.name}</li>
+          })}
+        </ul> */}
+        </>
+
+    );
 }
 export default Search;
